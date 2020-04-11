28763f3b707a8891d96c2d070046e6442f38cd68
RogerJRodriguez <rjr006@ucsd.edu>
2020-04-10 18:43:49 -0700

    Updates

---
+++ precache-manifest.4a69b3da58311d9318073f4a49f0d98a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b43bd2a1886fb3f704ef3b3905ff57",
+    "url": "/pokemon-data/index.html"
+  },
+  {
+    "revision": "b7e035c1e563e517565c",
+    "url": "/pokemon-data/static/css/2.8f77dfb0.chunk.css"
+  },
+  {
+    "revision": "b7e035c1e563e517565c",
+    "url": "/pokemon-data/static/js/2.bcd28a2c.chunk.js"
+  },
+  {
+    "revision": "0ac79d53dda6ffa0c6325c90109848cb",
+    "url": "/pokemon-data/static/js/2.bcd28a2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcc4f94b0e0f2fb57007",
+    "url": "/pokemon-data/static/js/main.130a8ba9.chunk.js"
+  },
+  {
+    "revision": "1b5beee9aca588152451",
+    "url": "/pokemon-data/static/js/runtime-main.6a0fc146.js"
+  }
+]);
